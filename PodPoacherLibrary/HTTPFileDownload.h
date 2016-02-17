2fdfe4c2c850686f2e532bd1b62ae1f05c8a049a
Benjamin Toynbee <toyners@hotmail.com>
2016-02-17 22:09:36 +1100

    Corrected the  callback type definition for use when the file downloading is happening.

--- PodPoacherLibrary/HTTPFileDownload.h
+++ PodPoacherLibrary/HTTPFileDownload.h
@@ -3,13 +3,12 @@
 #include "stdafx.h"
 #include <string>
 
-#define DLL __declspec(dllexport)
-typedef void(__stdcall * FileProgressCallback)(long filePosition);
+typedef void(*FileProgressCallback)(long);
 
 class HTTPFileDownload
 {
 public:
-  static DLL void downloadTextFile(std::string url, std::string filePath);
+  static void downloadTextFile(std::string url, std::string filePath);
 
-  static DLL void downloadBinaryFile(std::string url, std::string filePath, FileProgressCallback fileProgressCallback, int bufferSize = 512);
+  static void downloadBinaryFile(std::string url, std::string filePath, FileProgressCallback fileProgressCallback, int bufferSize = 512);
 };
